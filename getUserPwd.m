522b030b366a3929866fd9ef02d26f2dc34597f5
Baptiste MENETRIER <baptiste.menetrier1@gmail.com>
2022-04-30 18:25:33 +0200

    Reorganized folders

--- getUserPwd.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = getUserPwd(inputArg1,inputArg2)
-%GETUSERPWD Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
